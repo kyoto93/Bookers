58c9a58c4a3d4134fdd3aa4b09d2c3846f78ee03
kyoto93 <k12a24r@gmail.com>
2020-04-20 20:57:30 +0900

    再提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,13 +26,18 @@ class BooksController < ApplicationController
   end
 
   def edit
-  	@book = Book.find(params[:id])
+  	  @book = Book.find(params[:id])
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book.id)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+
+    else
+      render action: :edit
+    end
   end
 
   def destroy
